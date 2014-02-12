41092d3ee7332d653ac0d079c938758cfd51acca
Doroszlai Attila <eti@fene.hu>
2014-02-12 22:37:17 +0100

    unique colors

--- map.js
+++ map.js
@@ -11,6 +11,8 @@ function init_map() {
 
 	var routesLayer = L.featureGroup().addTo(map);
 	var routeCount = tracks.length;
+	var colors = unique_colors(routeCount);
+	var count = 0;
 	var loadedCount = 0;
 
 	tracks.forEach(function(track) {
@@ -18,7 +20,11 @@ function init_map() {
 			var route = new L.GPX(data, {
 				async: true,
 				marker_options: {},
-				polyline_options: { distanceMarkers: { lazy: true } },
+				polyline_options: {
+					color: colors[count++],
+					opacity: 0.75,
+					distanceMarkers: { lazy: true },
+				}
 			});
 			route.on('addline', function(evt) {
 				var polyline = evt.line;
@@ -49,7 +55,28 @@ function init_map() {
 	});
 }
 
+function unique_colors(count) {
+	if (count === 0) return [];
+	if (count === 1) return ['#0000ff'];
+	var increment = 1 / count;
+	var colors = [];
+	for (var i = 0; i < count; ++i) {
+		var hue = i * increment;
+		var rgb = hsvToRgb(hue, 1, 0.7);
+		var hex = '#' + rgbToHex(rgb[0], rgb[1], rgb[2]);
+		colors.push(hex);
+	}
+	return colors;
+}
+
+function rgbToHex(r, g, b) {
+	return byteToHex(r) + byteToHex(g) + byteToHex(b);
+}
+
+function byteToHex(n) {
+	return ((n >> 4) & 0x0F).toString(16) + (n & 0x0F).toString(16);
+}
+
 // TODO
 // labels instead of popups
-// unique color for each track
 // POIs
